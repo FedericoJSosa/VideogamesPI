d2bb82bd5d817e290d72d2f9627d730e2d9b9b8b
FedericoJSosa <federico13javiersosa@gmail.com>
2023-10-03 18:05:29 -0300

    PiD1

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genres } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Videogame.belongsToMany(Genres, { through: 'ActorMovies' });
+Genres.belongsToMany(Videogame, { through: 'ActorMovies' });
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
+      unique: true,
       allowNull: false,
     },
+    decription:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    platforms:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    img:{
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    releseDate:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    rating:{
+      type: DataTypes.INTEGER,
+      allowNull: false}
+
   });
 };
