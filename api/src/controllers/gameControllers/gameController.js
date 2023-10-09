e1dbe5c15c674695f205e227b010d889ef0d3fa1
FedericoJSosa <federico13javiersosa@gmail.com>
2023-10-09 01:13:03 -0300

    pi

--- api/src/controllers/gameControllers/gameController.js
+++ api/src/controllers/gameControllers/gameController.js
@@ -1,7 +1,7 @@
 const axios= require("axios");
 
 
- const gameController= async ()=>{
+const gameController= async ()=>{
     return await axios.get("https://api.rawg.io/api/games?key=c04b4a64ff0440188fe0868ada142f37")
     .then((response)=>{ return response.data})
     .catch(error=>{
