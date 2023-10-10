10e7b2a6d0ecc0c13e4af7435296f851ef9d004a
FedericoJSosa <federico13javiersosa@gmail.com>
2023-10-10 17:39:05 -0300

    asd

--- api/src/controllers/genreControllers/genreController.js
+++ api/src/controllers/genreControllers/genreController.js
@@ -4,10 +4,8 @@ const {Genre}= require("../../db");
 const genreController= async()=>{    
     if (await Genre.count() === 0){
         const response= await axios.get("https://api.rawg.io/api/genres?key=c04b4a64ff0440188fe0868ada142f37");
-        const newGenre= async(genreInfo)=>{
-                await Genre.bulkcreate(genreInfo)
-        };
-        newGenre(response);
+        const respData= response.data.results;
+        await Genre.bulkCreate(respData);
         const allGenre= await Genre.findAll();
         return allGenre;
     }
