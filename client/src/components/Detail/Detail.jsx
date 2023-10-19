3311dbd3225e31b5ac52863db3da11898df8ad3e
FedericoJSosa <federico13javiersosa@gmail.com>
2023-10-18 22:28:39 -0300

    boton

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { getVideogamesById } from "../../redux/actions";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Detail = () => {
     const { id } = useParams();
@@ -21,14 +22,15 @@ const Detail = () => {
         return(
             < div >
             
-                 <img src={videogamesId.background_image} alt="Not found" />
-                 <h2>Name: {videogamesId.name}</h2>
-                 <p>Genres: {videogamesId.genres.map(genres => genres.name).join(", ")}</p>
-                 <p>Platforms: {videogamesId.platforms.map(platforms => platforms.platform.name)}</p>
-                 <p>Description: {videogamesId.description_raw}</p>
-                 <p>Release Date: {videogamesId.released}</p>
+                <img src={videogamesId.background_image} alt="Not found" />
+                <h2>Name: {videogamesId.name}</h2>
+                <p>Genres: {videogamesId.genres.map(genres => genres.name).join(", ")}</p>
+                <p>Platforms: {videogamesId.platforms.map(platforms => platforms.platform.name)}</p>
+                <p>Description: {videogamesId.description_raw}</p>
+                <p>Release Date: {videogamesId.released}</p>
                 <p>Rating: {videogamesId.rating}</p>
-                
+                <br></br><br></br><br></br><br></br>
+                <Link to= "/home"><button>Atras</button></Link> 
             </div >
         )
     } else {
